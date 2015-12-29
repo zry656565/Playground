228d953afc860090d20d72552783382d358b8cc5
Jerry Zou <jerry.zry@outlook.com>
2015-12-29 23:45:52 +0800

    add more test cases for async.waterfall

--- async/test/waterfall.js
+++ async/test/waterfall.js
@@ -33,5 +33,93 @@ describe('waterfall', function() {
         done()
       })
     })
+    
+    it('empty array', function(done) {
+      async.waterfall([], function(err){
+        if (err) throw err
+        done()
+      })
+    })
+    
+    it('non-array', function(done) {
+      async.waterfall({}, function(err){
+        expect(err.message).to.eql('First argument to waterfall must be an array of functions')
+        done()
+      })
+    })
+    
+    it('no callback', function(done) {
+      async.waterfall([
+        function(callback){ callback() },
+        function(callback){
+          callback()
+          done()
+        }
+      ])
+    })
+    
+    it('async', function(done) {
+      var call_order = []
+      async.waterfall([
+        function(callback){
+          call_order.push(1)
+          callback();
+          call_order.push(2)
+        },
+        function(callback){
+          call_order.push(3)
+          callback()
+        },
+        function(){
+          expect(call_order).to.eql([1,2,3])
+          done()
+        }
+      ])
+    })
+    
+    it('error', function(done) {
+      async.waterfall([
+        function(callback){
+          callback('error')
+        },
+        function(callback){
+          assert.ok(false, 'next function should not be called')
+          callback()
+        }
+      ], function(err){
+        expect(err).to.eql('error')
+      })
+      setTimeout(done, 50)
+    })
+    
+    it('multiple callback calls', function(done) {
+      var call_order = [];
+      var arr = [
+        function(callback){
+          call_order.push(1)
+          // call the callback twice. this should call function 2 twice
+          callback(null, 'one', 'two')
+          callback(null, 'one', 'two')
+        },
+        function(arg1, arg2, callback){
+          call_order.push(2)
+          callback(null, arg1, arg2, 'three')
+        },
+        function(arg1, arg2, arg3, callback){
+          call_order.push(3)
+          callback(null, 'four')
+        },
+        function(/*arg4*/){
+          call_order.push(4)
+          arr[3] = function(){
+            call_order.push(4)
+            expect(call_order).to.eql([1,2,2,3,3,4,4])
+            done()
+          }
+        }
+      ]
+      async.waterfall(arr)
+    })
+    
   })
 })
